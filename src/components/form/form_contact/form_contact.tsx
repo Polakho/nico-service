43d84f020e8fed75a8fa41fbbee101a65cd8c504
Polakho <armatys.alex@gmail.com>
2022-04-11 23:15:14 +0200

    Style form_contact

--- src/components/form/form_contact/form_contact.tsx
+++ src/components/form/form_contact/form_contact.tsx
@@ -1,66 +1,63 @@
 import React from "react";
 import "./form_contact.css"
-import {Form, Button} from "react-bootstrap";
+import {Form, Button, FormCheck} from "react-bootstrap";
 
 const Form_contact: React.FC = () => {
+
+    function sendEmail() {
+        
+
+
+        return undefined;
+    }
 
     return (
         <section>
             <div className={"form-contact"}>
                 <div className={"form-contact-entete"}>
                     <h2>Contactez-nous</h2>
-                    <hr/>
+
                 </div>
                 <div className={"form-contact-body"}>
                     <p>Prenez contact avec nous à l'aide de ce formulaire, nous vous rappellerons dès que l'on pourra.</p>
                     <Form>
-                        <div className={"row"}>
-                            <label htmlFor="name" >Prénom</label>
-                            <input id={"name"} type="text" placeholder={"Prénom"} />
+                        <div className={"col-12 row group"}>
+                            <Form.Group className={"col-6 "}>
+                                <Form.Label htmlFor="name" >Prénom</Form.Label>
+                                <Form.Control id={"name"} type={"text"}  placeholder={"Marc"} />
+                            </Form.Group>
+                            <Form.Group className={"col-6"}>
+                                <Form.Label htmlFor="lastname" >Nom</Form.Label>
+                                <Form.Control id={"lastname"} type={"text"}  placeholder={"Dubois"} />
+                            </Form.Group>
                         </div>
-                        <div className={"row"}>
-                            <label htmlFor="lastname" >Nom</label>
-                            <input id={"lastname"} type="text" placeholder={"Nom"} />
+                        <div className={"col-12 row group"}>
+                            <Form.Group className={"col-6"}>
+                                <Form.Label htmlFor={"telephone"}>Téléphone</Form.Label>
+                                <Form.Control id={"telephone"} type={"tel"}  placeholder={"06 ** ** ** **"} />
+                            </Form.Group>
+                            <Form.Group className={"col-6"}>
+                                <Form.Label htmlFor={"mail"}>Émail</Form.Label>
+                                <Form.Control id={"email"} type={"mail"}  placeholder={"example@email.com"} />
+                            </Form.Group>
                         </div>
-                        <div className={"row"}>
-                            <label htmlFor={"telephone"}>Téléphone</label>
-                            <input id={"telephone"} type="tel" placeholder={"06XXXXXXXX"} />
+                        <div className={"col-12 row group"}>
+                            <Form.Group >
+                                <Form.Label htmlFor={"message"}>Message</Form.Label>
+                                <Form.Control as="textarea" id={"message"}  placeholder={"Décrivez votre projet..."} />
+                            </Form.Group>
                         </div>
-                        <div className={"row"}>
-                            <label htmlFor={"mail"}>Émail</label>
-                            <input id={"mail"} type="email" placeholder={"email@example.com"} />
+                        <div className={"col-12 row group"}>
+                            <Form.Group>
+                                <FormCheck type={"checkbox"} id={"politique-confidentialite"}
+                                           label={"En soumettant ce formulaire, j'accepte que les informations saisies soient traitées par Nico-Service"} />
+                            </Form.Group>
                         </div>
-                        <div className={"row"}>
-                            <label htmlFor={"message"}>Message</label>
-                            <textarea id={"message"}  placeholder={"Message"} />
-                        </div>
-                        <div className="row">
-                            <input id={"politique-confidentialite"} type="checkbox" />
-                            <label htmlFor={"politique-confidentialite"} >En soumettant ce formulaire, j'accepte que les informations saisies soient traitées par <strong>Nico-Service</strong></label>
-                        </div>
-                        <button id={"form-contact-send"}>Envoyer</button>
+                       <Button id={"form-contact-send"} variant="primary" onClick={sendEmail()} type="submit">
+                        Envoyer
+                       </Button>
                     </Form>
                 </div>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
             </div>
         </section>
 
